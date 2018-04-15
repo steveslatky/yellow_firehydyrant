253489dbf9cf1aeb2c6a8186f01fb8a356b067f5
Stephen Slatky <steve.slatky@gmail.com>
2018-04-14 21:59:37 -0400

    inersting, moving to desktop

---
+++ scripts/database_inserter.py
@@ -0,0 +1,42 @@
+import json
+import sqlite3
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return None
+
+
+def main():
+    f = open("../data/hydrants.json")
+    data = json.load(f)
+
+    conn = create_connection("../database/hydrants.db")
+    c = conn.cursor()
+    #c.execute('''CREATE TABLE locations (id int, lat float, lng float, oos bool, note varchar(255));''')
+
+    # {"lat":39.8852,"lng":-75.064254,"OutOfService":false,"Critical":false,"CriticalNotes":null}
+    for e, row in enumerate(data):
+        _lat = row["lat"]
+        _lng = row["lng"]
+        _oos = row["Critical"]
+        _note = row["CriticalNotes"]
+        c.execute('INSERT INTO locations VALUES(id,_lat,_lng,_oos,_note)')
+        conn.commit()
+
+
+
+if __name__ == '__main__':
+    main()
+
+
--- scripts/firehydrant.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-import numpy
-
-print("Hello World")
-=======
-import numpy
->>>>>>> a428985ec704ce2897274c80f1952a51dc5474cd
