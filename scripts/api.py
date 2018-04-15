58124b90c74e4fcd28175e8b5ba1927967ffb4f5
Stephen Slatky <steve.slatky@gmail.com>
2018-04-15 02:34:57 -0400

    api done

--- scripts/api.py
+++ scripts/api.py
@@ -1,38 +1,69 @@
 import googlemaps
+from haversine import haversine as hs
 import unittest
 
 import googlemaps
 
+'''
+Input: Address -> String
+Return: Json (
 
-def geocode(address):
+'''
+def _geocode(address):
     gmaps = googlemaps.Client(key="AIzaSyDx9ajo9RB833gTJzpLwZCI1TwvSww_Ywo")
     return gmaps.geocode(address)
 
 
+'''
+Input: Address -> String
+Return: Geo Coordinates of a address -> (Float, Float)
+
+'''
 def get_geo_cords(address):
-    json = geocode(address)
+    json = _geocode(address)
     lng = json[0]["geometry"]["viewport"]["southwest"]["lng"]
     lat = json[0]["geometry"]["viewport"]["southwest"]["lat"]
     return lng, lat
 
+'''
+Input: Address -> String
+Return: Float
 
+'''
 def get_lat(address):
     lng, lat = get_geo_cords(address)
     return lat
 
+'''
+Input: Address -> String
+Return: Float
 
+'''
 def get_lng(address):
     lng, lat = get_geo_cords(address)
     return lng
 
+'''
+Input: km -> Float
+Return: miles -> Float
 
+'''
 def _km_to_mi(km):
     conv_fac = 0.621371
     return km * conv_fac
 
 
-def get_distance():
-    pass
+'''
+Uses haversine formula to get distance with Geo coordinates
+
+Param param input (float, float), (float, float), Optional miles=True
+Return Float(miles/km) 
+
+'''
+def get_distance(query, input, miles=True):
+    hs(query,input,miles)
+
+
 
 
 class TestStringMethods(unittest.TestCase):
@@ -43,7 +74,7 @@ class TestStringMethods(unittest.TestCase):
     def test_get_distance(self):
         self.asserEquals(True,False)
 
-result = geocode("3400 lancaster avenue, Philadelphia, PA")
+result = _geocode("3400 lancaster avenue, Philadelphia, PA")
 
 print(result)
 
